2794fcc2bb972c4d39a7a1c3ee0b2776dedbc794
Douglas Ferreira <99023276+douglashdcreative@users.noreply.github.com>
2023-02-24 21:31:35 -0300

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+<div>
+  <Head>
+    <title>Homepage</title>
+  </Head>
+
+  <main>
+    <h1>Hello World</h1>
+  </main>
+</div>
+  )
+}
